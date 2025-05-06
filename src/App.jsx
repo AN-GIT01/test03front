794f801548702fd2d1cad1e5eea0f8bd26d632f6
AN <nickonov@mail.ru>
2025-05-07 00:50:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import EmptyComponent from './components/component'
+
+function App() {
+
+  return (
+    <>
+      <h1>Some text</h1>
+      <EmptyComponent/>
+    </>
+  )
+}
+
+export default App
